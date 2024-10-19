a0afddea7ae648a7e433a83157714a29af5d6281
Jan <jas.gol@poczta.onet.pl>
2024-10-19 18:07:51 +0200

    Add popup form and logic to add book

--- javascript.js
+++ javascript.js
@@ -1,5 +1,9 @@
 const library = [];
 
+const tbl = document.createElement("table");
+const tblHead = document.createElement("thead");
+const tblBody = document.createElement("tbody");
+
 function book(title, author, pages, read){
   this.title= title;
   this.author=author;
@@ -16,18 +20,24 @@ function book(title, author, pages, read){
   }
 }
 
-function addBookToLibrary(){
-  title=prompt("add title");
-  author=prompt("add author");
-  pages=prompt("add pages");
-  read=prompt("is book read?");
-  library.append(new book(title, author, pages, read));
+function addRow(tblBody, book){
+    const row = document.createElement("tr");
+    for(book_property of [book.title,book.author,book.pages]){
+        const cell = document.createElement("td");
+        cell.textContent = book_property;
+        row.appendChild(cell);
+    }
+    const cell = document.createElement("td");
+    if(book.read)
+        cell.textContent = 'Yes';
+    else
+    cell.textContent = 'No';
+    row.appendChild(cell);
+
+    tblBody.appendChild(row);
 }
 
-function displayBooks(books){
-    const tbl = document.createElement("table");
-    const tblHead = document.createElement("thead");
-    
+function displayBooks(books){   
     let head_elements = ['Title','Author','Pages','Read'];
     for (const element of head_elements){
         const cell = document.createElement("th");
@@ -35,33 +45,38 @@ function displayBooks(books){
         tblHead.appendChild(cell);
     }  
     
-    const tblBody = document.createElement("tbody");
-    
     for(const book of books){
-        const row = document.createElement("tr");
-        for(book_property of [book.title,book.author,book.pages]){
-            const cell = document.createElement("td");
-            cell.textContent = book_property;
-            row.appendChild(cell);
-        }
-        const cell = document.createElement("td");
-        if(book.read)
-            cell.textContent = 'Yes';
-        else
-            cell.textContent = 'No';
-        row.appendChild(cell);
-        tblBody.appendChild(row);
+        addRow(tblBody, book);
     }
     
     tbl.appendChild(tblHead);
     tbl.appendChild(tblBody);
-    
+
     document.body.appendChild(tbl);
+}
+
+function addBookToLibrary(properties){
+    library.push(new book(properties[0], properties[1], properties[2], false));
+    addRow(tblBody, library[library.length-1]);
 }
 
 library.push(new book('A Tale of Two Cities','Charles Dickens',123,true));
 library.push(new book('The Little Prince','Antonie de Saint Exupery',222,true));
 library.push(new book('The Alchemist','Paulo Coelho', 521, false));
-console.log(library);
 
 displayBooks(library);
+
+const addBookBtn = document.querySelector(".add-book");
+const dialog = document.querySelector("dialog");
+const confirmBtn = document.querySelector(".confirm-button");
+
+addBookBtn.addEventListener("click",(e)=>{
+    dialog.showModal();
+})
+
+dialog.addEventListener("close", (e)=>{
+    e.preventDefault();
+    let properties = Array.from(dialog.querySelectorAll("input")).map(input => input.value);
+    console.log(properties);
+    addBookToLibrary(properties);
+})
